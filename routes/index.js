93ea9346528488cfeac7973bb0a5998678e81e3a
Rick Evans <rdevansjr@earthlink.net>
2017-07-04 21:03:24 -0400

    Update index.js
    Changed to memory storage

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
+var storage = multer.memoryStorage();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'File Size' });
 });
 
-router.post('/get-file-size', multer({ dest: '../uploads/'}).single('file'), function(req, res, next) {
+router.post('/get-file-size', multer({ storage: storage}).single('file'), function(req, res, next) {
   res.json({"size":req.file.size});
 });
 
